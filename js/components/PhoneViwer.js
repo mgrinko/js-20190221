9725c12b5d77c0278dbe00996a63b8a4f003cb2a
Assanali <asanmaris@gmail.com>
2019-04-07 18:37:08 +0300

    Gallery added. Back button fixed

--- js/components/PhoneViwer.js
+++ js/components/PhoneViwer.js
@@ -1,56 +1,65 @@
 export default class PhoneViwer{
- constructor(element, props) {
-  this.element = element;
-  this.props = props;
+  constructor(element, props) {
+    this.element = element;
+    this.props = props;
+    this.render();
 
-  this.render();
+    const buttons = this.element.querySelectorAll('[data-element="Button"]');
 
-  this.element.addEventListener('click', (event) => {
-    const button = this.element.querySelector('[data-element="BackButton"]');
-    
-    this.props.onBack();
-  })
+    this.element.addEventListener('click', (event) => {
+      const elem = event.target.closest('[data-element="image"]');
+      if (!elem) {
+        return;
+      }
+      this.changeMainImage(elem.src);
+    });
 
- }
+    this.buttonActions(buttons);
 
- render() {
-  const { phone } = this.props;
+  }
 
-  this.element.innerHTML = `
-    <div>
-      <img class="phone" src="${ phone.images[0] }">
+  buttonActions(buttons) {
+    console.log(buttons);
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        if (button.dataset.action === "Back") {
+          this.props.onBack();
+        }
+      })
+    });
+  }
 
-      <button data-element="BackButton">Back</button>
-      <button>Add to basket</button>
+
+  changeMainImage(image) {
+    const mainImage = this.element.querySelector('.phone');
+    mainImage.src = image;
+  }
+
+  render() {
+    const { phone } = this.props;
+
+    this.element.innerHTML = `
+      <div>
+        <img class="phone" src="${ phone.images[0] }">
+
+        <button data-element="Button" data-action="Back">Back</button>
+        <button data-element="Button" data-action="Add">Add to basket</button>
 
 
-      <h1>${ phone.name }</h1>
+        <h1>${ phone.name }</h1>
 
-      <p>${ phone.description }</p>
+        <p>${ phone.description }</p>
 
-      <ul class="phone-thumbs">
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-        </li>
-      </ul>
+        <ul class="phone-thumbs">
+          ${phone.images.map(image => `
+          <li>
+            <img src="${image}" data-element="image">
+          </li>
+          `).join('')}
+        </ul>
 
-      
-    </div>
-  `
- }
+        
+      </div>
+    `
+  }
 }
