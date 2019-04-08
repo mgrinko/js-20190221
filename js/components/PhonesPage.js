82a571216d4da0131b510a988fb569498c4f1035
ibenkovytch <i.benkovytch@student.csn.khai.edu>
2019-04-08 18:22:17 +0300

    phones are now rendered from arr

--- js/components/PhonesPage.js
+++ js/components/PhonesPage.js
@@ -1,6 +1,7 @@
 import Filter from './Filter.js';
 import ShoppingCart from './ShoppingCart.js';
 import PhonesCatalog from './PhonesCatalog.js';
+import {getAll, getById} from '../api/phones.js';
 
 
 export default class PhonesPage {
@@ -9,15 +10,18 @@ export default class PhonesPage {
 
         this.render();
 
+        this.initComponent(PhonesCatalog, {
+            phones: getAll(),
+        });
 
-
-        this.initComponent(PhonesCatalog);
         this.initComponent(Filter);
         this.initComponent(ShoppingCart);
     }
 
-    initComponent(Constructor) {
-        new Constructor(this.element.querySelector(`[data-component="${Constructor.name}"]`));
+    initComponent(Constructor, props = {}) {
+        new Constructor(this.element.querySelector(`[data-component="${Constructor.name}"]`),
+        props
+        );
     }
 
     render() {
